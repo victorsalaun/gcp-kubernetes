cfa2d379ce5111886a93d4358a483aab16252ad1
Victor SALAUN <vsalaun@talend.com>
2020-02-20 00:04:17 +0000

    add admin autoscaling and management

--- terraform/cluster/cluster.tf
+++ terraform/cluster/cluster.tf
@@ -1,9 +1,11 @@
 variable "cluster_name" {}
 variable "cluster_location" {}
 
-variable "cluster_node_one_count" {}
-variable "cluster_node_one_machine_type" {}
-variable "cluster_node_one_name" {}
+variable "cluster_node_admin_count" {}
+variable "cluster_node_admin_machine_type" {}
+variable "cluster_node_admin_name" {}
+variable "cluster_node_admin_max_node_count" {}
+variable "cluster_node_admin_min_node_count" {}
 
 data "google_container_engine_versions" "gce_version_location" {
   location = var.cluster_location
@@ -22,6 +24,8 @@ resource "google_container_cluster" "cluster" {
   network    = data.terraform_remote_state.network.outputs.network_selflink
   subnetwork = data.terraform_remote_state.network.outputs.subnetwork_selflink
 
+  enable_legacy_abac = false
+
   master_auth {
     client_certificate_config {
       issue_client_certificate = false
@@ -29,15 +33,41 @@ resource "google_container_cluster" "cluster" {
   }
 }
 
-resource "google_container_node_pool" "cluster_node_one" {
-  name     = var.cluster_node_one_name
+resource "google_container_node_pool" "cluster_node_admin" {
+  name     = var.cluster_node_admin_name
   cluster  = google_container_cluster.cluster.name
   location = google_container_cluster.cluster.location
 
-  node_count = var.cluster_node_one_count
+  node_count = var.cluster_node_admin_count
+
+  autoscaling {
+    min_node_count = var.cluster_node_admin_min_node_count
+    max_node_count = var.cluster_node_admin_max_node_count
+  }
+
+  management {
+    auto_upgrade = false
+    auto_repair  = true
+  }
 
   node_config {
-    preemptible  = true
-    machine_type = var.cluster_node_one_machine_type
+    preemptible  = false
+    machine_type = var.cluster_node_admin_machine_type
   }
+}
+
+output "cluster_name" {
+  value = google_container_cluster.cluster.name
+}
+
+output "cluster_region" {
+  value = google_container_cluster.cluster.region
+}
+
+output "cluuster_endpoint" {
+  value = google_container_cluster.cluster.endpoint
+}
+
+output "cluster_node_version" {
+  value = google_container_cluster.cluster.node_version
 }
--- terraform/cluster/inputs.tfvars
+++ terraform/cluster/inputs.tfvars
@@ -6,9 +6,11 @@ cluster_location = "us-east1"
 cluster_name     = "kubernetes-cluster"
 
 #--------------------------------------------------------------
-# Cluster Node One
+# Cluster Node Admin
 #--------------------------------------------------------------
 
-cluster_node_one_count        = 1
-cluster_node_one_machine_type = "n1-standard-1"
-cluster_node_one_name         = "admin"
+cluster_node_admin_count        = 1
+cluster_node_admin_machine_type = "n1-standard-1"
+cluster_node_admin_name         = "admin"
+cluster_node_admin_max_node_count = 3
+cluster_node_admin_min_node_count = 1
